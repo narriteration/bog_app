3418e34c045bb176ce2fd30017b48c422d70b245
GA 3 <ga3@Taylors-MacBook-Air.local>
2017-04-24 15:30:28 -0700

    dropped db and reseeded. seed data is setup. index view for creatures works great

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Creature.create({name: "Yoda", description:"Wise small green alien"})
+Creature.create({name: "Anakin aka Darth Vader", description:"Cute kid, weirdo sadist adult"})
+Creature.create({name: "Yoda", description:"Warrior, diplomat, badass"})
+Creature.create({name: "Slug", description:"slimy"})
