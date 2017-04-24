a4aa1f172476ae540eaa37145cdbb69f2249c85a
GA 3 <ga3@Taylors-MacBook-Air.local>
2017-04-24 16:30:46 -0700

    update and edit routes working

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -9,7 +9,7 @@ class CreaturesController < ApplicationController
   end
 
   def create
-    creature_params = params.require(:creature).permit(:name, :description)
+    creature_params
     creature = Creature.new(creature_params)
     if creature.save
       redirect_to creature_path(creature)
@@ -19,6 +19,30 @@ class CreaturesController < ApplicationController
   def show
     creature_id = params[:id]
     @creature = Creature.find_by_id(creature_id)
+  end
+
+  def edit
+    creature_id = params[:id]
+    @creature = Creature.find_by_id(creature_id)
+  end
+
+  def update
+    ## get creature id and save to variable
+    creature_id = params[:id]
+    ## find creature by id
+    creature = Creature.find_by_id(creature_id)
+    ## whitelist params and save to variable
+    creature_params
+    ## update creature using update_attributes method, pass in creature params to grab updated info
+    creature.update_attributes(creature_params)
+    ## redirect to show page for the updated creature
+    redirect_to creature_path(creature)
+  end
+
+  private
+
+  def creature_params
+     params.require(:creature).permit(:name, :description)
   end
 
 end
