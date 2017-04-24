b6b00729243f07a45bf2c5805fe9846b0d51e6c3
GA 3 <ga3@Taylors-MacBook-Air.local>
2017-04-24 15:51:14 -0700

    new, show, create routes and views working

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'creatures#index'
-  get '/creatures', to: 'creatures#index', as: 'creatures' 
+  get '/creatures', to: 'creatures#index', as: 'creatures'
+  get '/creatures/new', to: 'creatures#new', as: 'new_creature'
+  post '/creatures', to: 'creatures#create'
+  get '/creatures/:id', to: 'creatures#show', as: 'creature'
 end
